876be1ed5040c8f1e551a8a1b2158a3eb5e4ac3d
bochen.li <bochen.li@xtalpi.com>
2023-12-26 10:17:27 +0800

    update torch ver to gpt

--- src/alffmc_torch.py
+++ src/alffmc_torch.py
@@ -1,121 +1,87 @@
 # -*- coding: utf-8 -*-
 import torch
-import numpy as np
+from itertools import product
 from torch import Tensor, BoolTensor, IntTensor
 
 
-def to_lower_triangle(matrix: Tensor) -> Tensor:
-    Q, R = torch.linalg.qr(matrix.T)
-    return R.T * torch.sign(torch.diag(R.T))
-
-
-def periodic_limit(fcoords: Tensor, matrix: Tensor, r_intra: float) -> tuple[Tensor, IntTensor]:
-    matinv = torch.linalg.inv(matrix)
-    unitcube = torch.Tensor([
-        [0, 0, 0],
-        [0, 0, 1],
-        [0, 1, 0],
-        [0, 1, 1],
-        [1, 0, 0],
-        [1, 0, 1],
-        [1, 1, 0],
-        [1, 1, 1]
-    ]).to(matrix.device)
-    coords = fcoords.inner(matrix)
-    c_max = coords.max(axis=0).values
-    c_min = coords.min(axis=0).values
-    c_span = c_max - c_min
-    cube = unitcube.repeat(2, 1, 1)
-    cube[0] = unitcube * c_span + c_min
-    cube[1] = cube[0] + r_intra * unitcube + r_intra * (unitcube - 1)
-    cube_outter = cube[1].inner(matinv)
-    period_range = torch.vstack([
-        (cube_outter.min(axis=0).values).floor().int(),
-        (cube_outter.max(axis=0).values).ceil().int()
-    ])
-    return cube, period_range
-
-
-def expand_symmerty(fcoords: Tensor, affine_matrix: Tensor) -> Tensor:
-    S = len(affine_matrix)
-    fc4 = torch.ones(len(fcoords), 4,
-                    dtype=fcoords.dtype,
-                    device=fcoords.device)
-    fc4[:, :3] = fcoords
-    fc = fc4.repeat(S, 1, 1)
-    for i in range(S):
-        torch.inner(fc[i], affine_matrix[i], out=fc[i])
-    fc = fc[:, :, :3]
-    fc -= fc.floor()
-    return fc
-
-
-def expand_period(fcoords: Tensor, period_range: IntTensor) -> Tensor:
-    cells = torch.cartesian_prod(
-        torch.arange(period_range[0, 0], period_range[1, 0]),
-        torch.arange(period_range[0, 1], period_range[1, 1]),
-        torch.arange(period_range[0, 2], period_range[1, 2]),
-    ).to(fcoords.device)
-    return fcoords.repeat(len(cells), 1, 1) + cells.reshape(len(cells), 1, 3)
-
-
-def ddosap(
+def alffmc_torch(
         fcoords: Tensor,
-        matrix: Tensor,
-        affine_matrix: Tensor,
-        symprec=1e-4,
-        r_inner=2.,
-        r_intra=3.,
-        as_vector_jj=False,
-    ) -> tuple[IntTensor, IntTensor, Tensor, IntTensor]:
-    """distance detector of symmetric and periodic system
-    对称和周期系统距离探测器
+        radius: Tensor,
+        lattice: Tensor,
+        affine_matrices: Tensor,
+        symprec=1e-4):
+    """
+    Atom linkage finder for molecule crystal using PyTorch.
 
     Args:
-        fcoords: (N, 3)  非对称单元的原子的分数坐标
-        matrix:  (3, 3)  周期性边界框
-        affine_matrix: (S, 4, 4) 对称操作的仿射变换矩阵
-        symmprec: (float) 重叠原子判断阈值
-        r_inner: (float) 非对称单元内连接距离
-        r_intra: (float) 非对称单元外连接距离
-        as_vector_jj: 使输出 vector_ij 改变为 j->j'
+        fcoords (torch.Tensor): Fractional coordinates of atoms in the asymmetric unit.
+        radius (torch.Tensor): Radii of atoms in the asymmetric unit.
+        lattice (torch.Tensor): Lattice vectors.
+        affine_matrices (torch.Tensor): Affine matrices of symmetry operations.
+        symprec (float, optional): Threshold for overlap atoms. Defaults to 1e-4.
 
-    Return:
-        index_i: (E)
-        index_j: (E)
-        vector_ij: (E, 3) 向量 i -> j'
-        conn_type: (E)
+    Returns:
+        tuple: A tuple containing the indices and vectors of the colliding atoms.
     """
-    N = len(fcoords)
-    S = len(affine_matrix)
-    matrix = to_lower_triangle(matrix)
-    cube, period_range = periodic_limit(fcoords, matrix, r_intra)
-    fc = expand_symmerty(fcoords, affine_matrix).reshape(-1, 3)
-    fc = expand_period(fc, period_range).reshape(-1, 3)
+    natom = radius.size(0)
+    nsym = affine_matrices.size(0)
+    assert radius.size(0) == fcoords.size(0)
+    assert lattice.size() == (3, 3)
+    assert affine_matrices.size() == (nsym, 4, 4)
 
-    index1 = origin_symmop_index(affine_matrix)
-    mask1 = torch.arange(N*index1, N*(index1+1))
-    index2 = origin_period_index(period_range)
-    mask2 = torch.arange(N*S*index2, N*S*(index2+1))
-    mask = mask2[mask1]
-    c = fc.inner(matrix)
+    # QR decomposition in PyTorch
+    q, r = torch.linalg.qr(lattice.T)
+    mat = r.T
+    matinv = torch.linalg.inv(mat)
+    matdiag = torch.diag(mat)
 
-    # coords3: atoms out cube, in cube+r_max
-    mask3 = np.all((cube_out[0] <= fc), axis=1) * np.all((coords3 < cube_out[-1]), axis=1)
-    coords3 = coords3[mask3]
-    index3 = np.tile(np.arange(N), S * C)[mask3]
-    radius3 = r_inner
+    # unitcell
+    fc4 = torch.ones((natom, 4), dtype=fcoords.dtype, device=fcoords.device)
+    fc4[:, :3] = fcoords
+    unit_fcoords = torch.cat([torch.matmul(fc4, a)[:, :3] for a in affine_matrices])
+    unit_fcoords -= torch.floor(unit_fcoords)
+    unit_coords = torch.matmul(unit_fcoords, mat)
 
-    # coords2: atoms in cube
-    mask2 = np.all((cube_in[0] <= coords3), axis=1) * np.all((coords3 < cube_in[-1]), axis=1)
-    coords2 = coords3[mask2]
-    index2 = index3[mask2]
-    radius2 = r_intra
-    assert coords2.shape[0] == S * N
+    # rectangular box
+    r_max = torch.max(radius) * 2
+    box = torch.tensor(list(product([0, 1], repeat=3)), dtype=fcoords.dtype, device=fcoords.device)
+    ibox = box * matdiag
+    obox = ibox + r_max * box + r_max * (box - 1)
+    obox_frac = torch.matmul(obox, matinv)
 
-    # collision test
-    idx, vec = detectDistance(coords2, coords3, radius2, radius3, symprec=symprec)
-    idx[:, 0] = index2[idx[:, 0]]
-    idx[:, 1] = index3[idx[:, 1]]
-    return idx, vec
+    # smallest supercell containing all atoms in a rectangular box
+    pmin = torch.floor(torch.min(obox_frac, dim=0).values).to(torch.int)
+    pmax = torch.ceil(torch.max(obox_frac, dim=0).values).to(torch.int) + 1
+    ncell = torch.prod(pmax - pmin)
+    frac_offset = torch.stack(torch.meshgrid([torch.arange(a, b) for a, b in zip(pmin, pmax)]), dim=-1).view(-1, 3)
+    cart_offset = torch.matmul(frac_offset, mat)
+    super_coords = unit_coords.repeat(ncell, 1)
+    super_coords += cart_offset.repeat_interleave(nsym * natom, dim=0)
 
+    mask1 = torch.all((obox[0] <= super_coords), dim=1) & torch.all((super_coords < obox[-1]), dim=1)
+    coords1 = super_coords[mask1]
+    index1 = torch.arange(natom).repeat(nsym * ncell)[mask1]
+    radius1 = radius[index1]
+
+    mask2 = torch.all((ibox[0] <= coords1), dim=1) & torch.all((coords1 < ibox[-1]), dim=1)
+    coords2 = coords1[mask2]
+    index2 = index1[mask2]
+    radius2 = radius1[mask2]
+
+    assert coords2.size(0) == nsym * natom, f'{coords2.size(0)} != {nsym * natom}'
+
+    index, vector = collisionDetect_torch(coords1, coords2, radius1, radius2, symprec=symprec)
+    index[:, 0] = index1[index[:, 0]]
+    index[:, 1] = index2[index[:, 1]]
+    return index, vector
+
+
+def collisionDetect_torch(coords1: Tensor, coords2: Tensor, radius1: Tensor, radius2: Tensor, symprec=1e-4) -> tuple[Tensor, Tensor]:
+    r2 = (radius1.view(-1, 1) + radius2)**2
+    d2 = -2 * torch.matmul(coords1, coords2.t()) \
+        + torch.sum(coords1**2, dim=1).view(-1, 1) \
+        + torch.sum(coords2**2, dim=1)
+    mask = (symprec**2 < d2) * (d2 < r2)
+    index = torch.nonzero(mask)
+    vector = coords2[index[:, 1]] - coords1[index[:, 0]]
+    return index, vector
